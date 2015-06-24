7358afc8e26b86960abd20fcf139938184127f65
ratatatKE <moses.ndeda@strathmore.edu>
2015-06-24 11:15:04 +0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,12 @@ from . import views
 urlpatterns = [
 		#an empty string will match for here
 		url(r'^$', views.post_list),
+		# this is a url for viewing only one post
+		url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail)
+		# ^ is the beginning
+		#after the beginnign the url should contain the word post
+		# (?P<pk>[0-9]+) Django should collect everything here and transfer it to the view as a variable called pk
+		#0-9 it can only be anomber
+		# + there needs to be one oe more digits
+		#$ means the end
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 #import data form the Post Model
 from .models import Post
@@ -12,3 +12,10 @@ def post_list(request):
 	posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
  	#to pass posts to the user, we will pass it to the template as views {'posts':posts}
  	return render(request, 'blog/post_list.html', {'posts':posts})
+
+ 	#we have a new view called post detail, it is getting a variable called pk 
+def post_detail(request, pk):
+ 	#we want to get one and only one object from our blog post
+	post=get_object_or_404(Post, pk=pk)
+	#return it to the client
+	return render(request, 'blog/post_detail.html', {'post':post})
